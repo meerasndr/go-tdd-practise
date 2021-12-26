01cff3c1ed06116f720f6bc9ddb1d4d186b35df6
meerasndr <mirage.sundial@gmail.com>
2021-12-26 11:43:55 +0530

    .

--- 04_arrays_slices/sum.go
+++ 04_arrays_slices/sum.go
@@ -1,5 +1,6 @@
 package main
-func SumArr(numbers [5] int) int{
+
+func SumArr(numbers [5]int) int {
 	sum := 0
 	for _, num := range numbers {
 		sum += num
@@ -13,4 +14,25 @@ func SumSlice(numbers []int) int {
 		sum += num
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, SumSlice(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ... []int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		}else {
+			tail := numbers[1:]
+			sums = append(sums, SumSlice(tail))
+		}
+	}
+	return sums
 }
--- 04_arrays_slices/sum_test.go
+++ 04_arrays_slices/sum_test.go
@@ -1,6 +1,15 @@
 package main
-import "testing"
 
+import (
+	"reflect"
+	"testing"
+)
+
+func AssertSumDeepEqual(got , want []int, t *testing.T){
+	if !reflect.DeepEqual(got, want){
+		t.Errorf("got %v want %v", got, want)
+	}
+}
 func TestSumArr(t *testing.T){
 	numbers := [5]int{1, 2, 3, 4, 5}
 	got := SumArr(numbers)
@@ -18,4 +27,26 @@ func TestSumSlice(t *testing.T){
 	if got != want {
 		t.Errorf("got %d want %d, for given %v", got, want, numbers)
 	}
+}
+
+func TestSumAll(t *testing.T){
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	if !reflect.DeepEqual(got, want){
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T){
+	t.Run("make the sums of some slices", func(t * testing.T){
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		AssertSumDeepEqual(got, want, t)
+
+	})
+	t.Run("safely sum empty slices", func(t *testing.T){
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		AssertSumDeepEqual(got, want, t)
+	})
 }
